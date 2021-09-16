89349348611b06b92247a1640f06672ef4969db8
Mahesh Makani <mahesh.makani@guardian.co.uk>
2021-09-16 15:45:55 +0100

    feat: init project and add mp environment voting scraper

---
+++ src/mpVotes.ts
@@ -0,0 +1,43 @@
+import got, { Response } from 'got';
+import { JSDOM } from 'jsdom';
+
+const getUrl = async (url: string) => got(url, { followRedirect: true });
+
+const getDom = (response: Response<string>) => new JSDOM(response.body);
+
+const getMPVotesDom = async (url: string) => {
+  const response = await getUrl(`${url}/votes`);
+  return getDom(response);
+}
+
+const cleanTextEnvironmentVote = (textContent: string | null) => {
+  const split = textContent?.split('\n').map(str => str.replace('Show votes', '').trim()).filter(Boolean);
+  if (split)
+    return [split[0] + '.', split[1]];
+  else
+    return ['', ''];
+}
+
+const getEnvironmentVotes = (dom: JSDOM) => {
+  const votes: string[][] = [];
+  let title = '';
+  const environmentElement = dom.window.document.getElementById('environment');
+  const parentElement = environmentElement?.parentElement;
+  if (environmentElement && parentElement) {
+    title = environmentElement.textContent?.replace('#', '').trim() || '';
+    const votesList = parentElement?.querySelectorAll('li');
+    if (votesList.length) {
+      votesList.forEach(elem => votes.push(cleanTextEnvironmentVote(elem.textContent)));
+    }
+  }
+  return {
+    title,
+    votes,
+  };
+}
+
+export const getEnvironmentVotesMPByPostcode = async (postcode: string) => {
+  const mpUrlResponse = await getUrl(`https://www.theyworkforyou.com/postcode/?pc=${postcode}`)
+  const mpVotesDom = await getMPVotesDom(mpUrlResponse.url);
+  return getEnvironmentVotes(mpVotesDom)
+}
